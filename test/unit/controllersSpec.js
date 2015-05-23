ca4eb1e72c015c72941b97e419dc515e779b1414
Ian Kosen <i@ikosenn.me>
2015-05-24 01:16:03 +0300

    Add a list of 20 phones
    Use of Angular http service to fetch list of mobile phones using http service

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,19 +2,23 @@
 
 describe('PhoneCat controllers', function(){
     describe('PhoneListCtrl', function(){
-        var scope, ctrl
+        var scope, ctrl, $httpBackend;
         beforeEach(module('PhonecatApp'));
 
-        beforeEach(inject(function($controller){
-                var scope = {},
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+
+                $httpBackend = _$httpBackend_;
+                $httpBackend.exceptGET('phones/phones.json').respond(
+                    [{name: 'Nexus S'}, {name:'Motorola DROID'}]);
+                scope = $rootScope.$new();
                 ctrl = $controller('PhoneListCtrl', {$scope:scope});
             }));
-        it('should create "phones"  model with 3 phones', function(){
-            except(scope.phones.length).toBe(3);
-        });
 
-        it('should set the default value of oerderProp model', function(){
-            except(scope.oerderProp).toBe('age');
+        it('should create "phones" model with 2 phones fetched from xhr', function(){
+            except(scope.phones).toBeUndefined();
+            $httpBacked.flush();
+            except(scope.phones).toEqual([{name:'Nexus S'},
+                                            {name:'Motorola DROID'}]);
         });
     });
 });
